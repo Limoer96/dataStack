118742027b0db2de5ed6e57d62d7aa61629896f9
limoer <myt19960503@outlook.com>
2017-12-06 21:16:18 +0800

    完成行为学生数据的模拟，接下来设计API

--- mock/behavior.js
+++ mock/behavior.js
@@ -1,5 +1,11 @@
 const Behavior = require('../model/Behavior');
 const getMenber = require('../util/getstudentbyexperimentid');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
 
 function genOneBehavior(experiment, user) {
 	const s_id = user.s_id;
@@ -40,6 +46,50 @@ function addBehaviors(experiment, menber) {
 	console.log('耗时');
 }
 
-getMenber(id, addBehaviors);
+// getMenber(id, addBehaviors);
+
+
+
+function addBehaviorsForUser(userid) {
+		Behavior.findById(userid, function(err, user) {
+			if(err) {
+				throw new Error(err.message);
+			}else {
+				let behaviors = user.behaviors;
+				const date = new Date(2017, Math.round(Math.random() * 2) + 9, Math.round(Math.random() * 29) + 1, Math.round(Math.random() * 13) + 9, Math.round(Math.random() * 59));
+				const offset = (Math.round(Math.random() * 220) + 20) * 60 * 1000 // 学习时间是20分钟到240分钟不等
+				behaviors.addToSet({
+					e_id: behaviors[0].e_id,
+					operating_category: Math.random()/8 > 0.1 ? 3 : 2,
+					start_time: date.getTime(),
+					end_time: new Date(date.getTime() + offset).getTime() 
+				})
+				user.markModified('behaviors');
+				user.save(function(err) {
+					if(err) {
+						console.log('发生错误了');
+					}else {
+						// console.log('插入行为成功');
+					}
+				})
+			}
+		})
+}
+
+rl.question('输入待插入行为条数', (value) => {
+	const count = Number(value);
+	console.time('耗时');
+	for(let i = 0; i < count; i++) {
+		addBehaviorsForUser('5a269fe3a615da55391c5158');
+	}
+	console.timeEnd('耗时');
+})
+
+
+
+
+
+
+
 
 
--- model/Behavior.js
+++ model/Behavior.js
@@ -5,7 +5,7 @@ var behaviorSchema = mongoose.Schema({
 	name: { type: String, required: true },
 	behaviors: [{
 		e_id: { type: Number, required: true },
-		operating_category: { type: Number, required: true }, // 操作类别：实验预约，实验学习，成果提交
+		operating_category: { type: Number, required: true }, // 操作类别：1. 实验预约，2. 实验学习，3. 成果提交
 		start_time: { type: Date, default: Date.now },
 		end_time: { type: Date }
 	}]
